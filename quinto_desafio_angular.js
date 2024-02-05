6b9dec98f58245bcb68338b7858dbbed2d79d8e8
oasosvaldo <oasosvaldo@gmail.com>
2024-02-05 15:59:27 -0300

    Update

--- quinto_desafio_angular.js
+++ quinto_desafio_angular.js
@@ -1,25 +1,36 @@
 /* Criar um algoritmo que calcule o valor do produto
-mediante a forma de pagamento do cliente.
+mediante a forma de pagamento do cliente usando funções.
 Ex:
 1- A vista no debito - desconto de 10%
 2- A vista dinheiro ou pix - desconto de 15%
 3- Em 2X - sem juros
 4- Acima de 2X - juros de 10%
 */
+
+function aplicarDesconto(preco, desconto) {
+    return preco - (preco * (desconto / 100))
+}
+
+function aplicarJuros(preco, juros) {
+    return preco + (preco * (juros / 100))
+}
+
+
+
 var readlinesync = require('readline-sync')
-var preco = parseFloat(readlinesync.question('Preço da etiqueta: '))
+var precoEtiqueta = parseFloat(readlinesync.question('Valor da etiqueta: '))
 var formaDePagamento = parseInt(readlinesync.question('Forma de pagamento: '))
 
 if (formaDePagamento === 1) {    
-    preco = (preco-(preco*0.10))
+    preco = (aplicarDesconto(precoEtiqueta, 10))
     console.log(preco)
 } else if (formaDePagamento === 2) {
-    preco = (preco-(preco*0.15))
+    preco = (aplicarDesconto(precoEtiqueta, 15))
     console.log(preco)
 }else if (formaDePagamento === 3) {
     console.log(preco)
 } else if (formaDePagamento === 4) {
-    preco = (preco+(preco*0.10))
+    preco = (aplicarJuros(precoEtiqueta, 10))
     console.log(preco)
 } else {
     console.log('Forma de pagamento invalida')
